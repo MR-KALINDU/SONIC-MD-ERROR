f049c8e720200d275d701d1c4e432c73008442ba
KALIDUWA OFC ✔️ <132440655+KALINDU-LK@users.noreply.github.com>
2023-07-27 18:52:43 +0530

    💜 SONIC-MD 💚

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,15 +1,1 @@
-const {cmd } = require('../lib')
-cmd({
-        pattern: "insta",
-        desc: "download instagram post.",
-        category: "downloader",
-        filename: __filename
-    },
-    async(Void, citel,text,{isCreator}) => {
-        const { Insta } = require('../lib')
-if(!text) return citel.reply('Need post url.')
-let response = await Insta(text)
-for (let i=0;i<response.length;i++) {
-await Void.sendFileUrl(citel.chat, response[i], `*Downloaded Media from instagram.*`, citel)
-}
-    });
+function _0x172a(_0xc8443c,_0x17468f){const _0x4c2e20=_0x4c2e();return _0x172a=function(_0x172a63,_0x259f45){_0x172a63=_0x172a63-0xe2;let _0x4ce6cf=_0x4c2e20[_0x172a63];return _0x4ce6cf;},_0x172a(_0xc8443c,_0x17468f);}const _0x111ade=_0x172a;(function(_0x3689a9,_0xea84ab){const _0x46fac3=_0x172a,_0x30e48c=_0x3689a9();while(!![]){try{const _0x2489df=-parseInt(_0x46fac3(0xed))/0x1*(-parseInt(_0x46fac3(0xf1))/0x2)+parseInt(_0x46fac3(0xf2))/0x3+parseInt(_0x46fac3(0xf3))/0x4*(parseInt(_0x46fac3(0xe9))/0x5)+-parseInt(_0x46fac3(0xe8))/0x6*(-parseInt(_0x46fac3(0xe4))/0x7)+parseInt(_0x46fac3(0xea))/0x8*(-parseInt(_0x46fac3(0xec))/0x9)+-parseInt(_0x46fac3(0xf0))/0xa+parseInt(_0x46fac3(0xe2))/0xb*(parseInt(_0x46fac3(0xef))/0xc);if(_0x2489df===_0xea84ab)break;else _0x30e48c['push'](_0x30e48c['shift']());}catch(_0x561d68){_0x30e48c['push'](_0x30e48c['shift']());}}}(_0x4c2e,0x524e4));const {cmd}=require(_0x111ade(0xeb));function _0x4c2e(){const _0x38e0c3=['176eaZvXa','downloader','28hBpryo','insta','length','sendFileUrl','80340OXnVOD','70kUNnLw','40ypplHk','../lib','175563GIlsKC','24113ijkAlv','*Downloaded\x20Media\x20from\x20instagram.*','140376oWGCFA','1560690mzLrxO','4xCkRPY','581124yOXHmF','30876Mlcjmv'];_0x4c2e=function(){return _0x38e0c3;};return _0x4c2e();}cmd({'pattern':_0x111ade(0xe5),'desc':'download\x20instagram\x20post.','category':_0x111ade(0xe3),'filename':__filename},async(_0x3a794e,_0x198474,_0x4e809e,{isCreator:_0x5975ae})=>{const _0x32e0ad=_0x111ade,{Insta:_0x3c6a7b}=require(_0x32e0ad(0xeb));if(!_0x4e809e)return _0x198474['reply']('Need\x20post\x20url.');let _0x5a2b20=await _0x3c6a7b(_0x4e809e);for(let _0x18fa5b=0x0;_0x18fa5b<_0x5a2b20[_0x32e0ad(0xe6)];_0x18fa5b++){await _0x3a794e[_0x32e0ad(0xe7)](_0x198474['chat'],_0x5a2b20[_0x18fa5b],_0x32e0ad(0xee),_0x198474);}});

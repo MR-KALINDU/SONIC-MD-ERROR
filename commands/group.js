b4ff83cf716216188dee94f280472be6be97499f
KALIDUWA OFC ✔️ <132440655+KALINDU-LK@users.noreply.github.com>
2023-07-28 20:48:19 +0530

    💜 SONIC-MD 💚

--- commands/group.js
+++ commands/group.js
@@ -90,10 +90,10 @@ cmd({
         filename: __filename,
     },
     async(Void, citel, text) => {
-        citel.reply(`*Check your Pm ${tlang().greet}*`);
+        citel.reply(`*CHECK YOUR INBOX ${tlang().greet}*`);
         await Void.sendMessage(`${citel.sender}`, {
             image: log0,
-            caption: `*Group Name: Secktor-Support*\n*Group Link:* https://chat.whatsapp.com/Bl2F9UTVU4CBfZU6eVnrbC`,
+            caption: `*GROUP NAME: SONIC-MD-SUPPORT*\n\n*GROUP LINK :* https://chat.whatsapp.com/LkP8QZqdhXUKxmc0hUPip5`,
         });
 
     }

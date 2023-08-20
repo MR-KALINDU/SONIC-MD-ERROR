a8da423f0402d8afbf210746ca39a2e39511bb39
sahil0king03 <142722988+sahil0king03@users.noreply.github.com>
2023-08-20 22:10:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ global.email = 'sam@secktor.live'
 global.github = 'https://github.com/SamPandey001/Secktor-Md'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '919628516236'
+global.sudo = process.env.SUDO || '923447180760'
 global.devs = '919628516236';
-global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+91000000000000
+global.website = 'https://github.com/SamPandey001/Secktor-Md' //wa.me/+923447180760
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png'
 module.exports = {
   botname:   process.env.BOT_NAME === undefined ? '𝐒𝐞𝐜𝐤𝐭𝐨𝐫 𝐁𝐨𝐭𝐭𝐨' : process.env.BOT_NAME,
